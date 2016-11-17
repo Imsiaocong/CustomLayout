ce6b964d8d9e2b288706d00684f590dfb09569c8
王笛 Wang,Di <wd1998313@gmail.com>
2016-11-17 00:41:34 -0500

    Add contents

--- CustomLayout/ViewController.swift
+++ CustomLayout/ViewController.swift
@@ -13,6 +13,7 @@ class CollectionViewController: UIViewController {
     var collectionView: UICollectionView!
     var barHeight: CGFloat = 200.0
     var textField: DetailView!
+    var getColor: Float!
     
     fileprivate var viewRect: CGRect!
     fileprivate var cellIdentifier = "cellIdentifier"
@@ -22,7 +23,7 @@ class CollectionViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         self.viewRect = CGRect(x: 0, y: 0, width: self.view.bounds.width, height: 200)
         self.collectionView = UICollectionView(frame: viewRect, collectionViewLayout: FlowLayout())
-        self.collectionView.backgroundColor = UIColor.yellow
+        self.collectionView.backgroundColor = UIColor(colorLiteralRed: 0.0, green: 1.0, blue: 1.0, alpha: 1.0)
         self.collectionView.dataSource = self
         self.collectionView.delegate = self
         self.collectionView.register(CustomCollectionViewCell.self, forCellWithReuseIdentifier: cellIdentifier)
@@ -40,7 +41,7 @@ class CollectionViewController: UIViewController {
     func drawDetailView() {
         self.textField = DetailView(frame: CGRect(x: 0, y: barHeight, width: self.view.frame.size.width, height: self.view.frame.size.height - barHeight))
         self.textField.textDetail = "Detailssss!"
-        self.textField.detailText.backgroundColor = UIColor.yellow
+        self.textField.detailText.backgroundColor = UIColor(colorLiteralRed: 0.0, green: 1.0, blue: 1.0, alpha: 1.0)
         self.view.addSubview(textField)
     }
 
@@ -51,7 +52,7 @@ class CollectionViewController: UIViewController {
 
 extension CollectionViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return 6
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -66,11 +67,48 @@ extension CollectionViewController: UICollectionViewDelegate, UICollectionViewDa
 extension CollectionViewController {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath) as! CustomCollectionViewCell
-        print("\(cell.frame.origin.x + (cell.frame.size.width / 2))" + " \(collectionView.center.x)")
+        print(cell.frame.origin.x)
     }
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        //
+        //Statements below:
+        print(scrollView.contentOffset.x)
+        
+        let position_x: Float = Float(scrollView.contentOffset.x)
+        let redElement: Float = position_x / 850.0
+        
+        if position_x >= 0.0 && position_x < 170.0 {
+            UIView.animate(withDuration: 0.5, animations: {
+                self.textField.detailText.backgroundColor = UIColor(colorLiteralRed: redElement, green: 1.0, blue: 1.0, alpha: 1.0)
+            })
+        }
+        if position_x >= 170.0 && position_x < 340.0 {
+            UIView.animate(withDuration: 0.5, animations: {
+                self.textField.detailText.backgroundColor = UIColor(colorLiteralRed: redElement, green: 1.0, blue: 1.0, alpha: 1.0)
+            })
+        }
+        if position_x >= 340.0 && position_x < 510.0{
+            UIView.animate(withDuration: 0.5, animations: {
+                self.textField.detailText.backgroundColor = UIColor(colorLiteralRed: redElement, green: 1.0, blue: 1.0, alpha: 1.0)
+            })
+        }
+        if position_x >= 510.0 && position_x < 680.0{
+            UIView.animate(withDuration: 0.5, animations: {
+                self.textField.detailText.backgroundColor = UIColor(colorLiteralRed: redElement, green: 1.0, blue: 1.0, alpha: 1.0)
+            })
+        }
+        if position_x >= 680.0 && position_x < 850.0{
+            UIView.animate(withDuration: 0.5, animations: {
+                self.textField.detailText.backgroundColor = UIColor(colorLiteralRed: redElement, green: 1.0, blue: 1.0, alpha: 1.0)
+            })
+        }
+        if position_x >= 850.0 {
+            UIView.animate(withDuration: 0.5, animations: {
+                self.textField.detailText.backgroundColor = UIColor(colorLiteralRed: redElement, green: 1.0, blue: 1.0, alpha: 1.0)
+            })
+        }
+        
+        self.collectionView.backgroundColor = UIColor(colorLiteralRed: redElement, green: 1.0, blue: 1.0, alpha: 1.0)
         
     }
 }
